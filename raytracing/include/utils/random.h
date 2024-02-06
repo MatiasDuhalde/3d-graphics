09ba91747ec7a78fd45b4bdea1f5c934c4027550
Matías Duhalde <matias.duhalde@uc.cl>
2024-02-06 23:13:01 +0100

    docs: add fixme comment

--- raytracing/include/utils/random.h
+++ raytracing/include/utils/random.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <random>
 
+// FIXME: std::default_random_engine is not thread safe
 static std::random_device randomDevice;
 static std::default_random_engine randomEngine(randomDevice());
 static std::uniform_real_distribution<double> randomDistribution = std::uniform_real_distribution<double>(0.0, 1.0);
